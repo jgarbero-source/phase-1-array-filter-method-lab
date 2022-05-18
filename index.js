e68db410e79c56344e3a91a1f4cdba0f77e89bb9
John Garbero <john.garbero.itb@gmail.com>
2022-05-18 10:39:36 -0500

    commit tree 4d8ee30afede55dca9097213c9255e37fa605809

--- index.js
+++ index.js
@@ -1,1 +1,92 @@
 // Code your solution here
+
+// function findMatching(drivers, str) {
+//     return str === str;
+// }
+
+// const findMatching = filter(drivers, function (driver) {
+//     return driver === 
+// }
+
+// const findMatching = filter(drivers, function (str) {
+//     return str === "Bobby"; 
+// })
+
+// function findMatching(drivers, str) {
+//     return str === "Bobby";
+// }
+
+// function findMatching(drivers, str) {
+//     drivers.filter()
+// }
+
+// const findMatching = drivers.filter(drivers => str)
+
+// const findMatching = 
+
+// drivers.filter(function (name){
+//     return array.
+// })
+
+
+// array.filter(function (something something ){return name === something something something});
+
+// function findMatching(array, str) {
+//     return array.filter(str.toLowerCase) === str.toLowerCase;
+// }
+
+// const findMatching = filter(array, function (str) {
+//     return str.toLowerCase = array.str.toLowerCase;
+// })
+
+// str === str
+
+// function findMatching(array, string) {
+//     array.filter(function (index) {
+//         if (index === string) {
+//             return index
+//         }
+//     })
+// }
+
+// function findMatching(drivers, name){
+//     const solution = drivers.filter(function(person){
+//         return person.toLowerCase() === name.toLowerCase()
+//     })
+// }
+
+function findMatching(array, name) {
+    let match = array.filter(driverName => driverName.toLowerCase() === name.toLowerCase());
+    return match;
+}
+findMatching('Bobby');
+
+// function fuzzyMatch(drivers, str) {
+//     let match = drivers.filter(driverNames => driverNames.startsWith() === str);
+//     return match
+// }
+
+function fuzzyMatch(array, str) {
+    let match = array.filter(driverNames => driverNames.startsWith(str));
+    return match;
+}
+
+function matchName(array, name) {
+    let match = array.filter(driverNames => driverNames.name === name)
+    return match;
+}
+
+// function fuzzyMatch(array, str) {
+//     let match = array.filter(driverNames => driverNames.startsWith() === str.startsWith())
+//     return match;
+// }
+
+
+// function findMatching(drivers, name) {
+//     let match = drivers.filter(function (driverName) {
+//         driverName.toLowerCase() === name.toLowerCase();
+//         return match
+//     })
+// }
+
+// findMatching('Bobby')
